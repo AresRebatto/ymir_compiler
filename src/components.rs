e63af080630fd4dd5d95720344b635a7323ffbaf
AresRebatto <aresrebatt@gmail.com>
2024-10-12 08:58:09 +0200

    Implementazione enums per il salvataggio delle regole grammaticali del linguaggio localmente. Scritto firma per il metodo per la deserializzazione delle regole passate come stringa e per l'unwrap dall'enum

--- src/components.rs
+++ src/components.rs
@@ -1,4 +1,7 @@
 pub mod lexer;
 pub mod parser;
+pub mod grammar_rule;
 
+//Contiene la struct per salvare
+//i dati dallo yaml
 mod grammar;
---
+++ src/components/grammar_rule.rs
@@ -0,0 +1,47 @@
+use super::{lexer::*, parser::*};
+
+//Serve per contenere le singole regole della
+//grammatica del linguaggio
+#[derive(Clone, Debug)]
+pub enum GrammarRule{
+    Raw(String),
+    Worked(Vec<GrammarRuleSec>)
+}
+
+impl GrammarRule{
+    pub fn new(raw_rule: String) -> Self{
+        Self::Raw(raw_rule)
+    }
+
+    //Quando le regole vengono scaricate, sono in
+    //un formato stringa. Questa funzione le deserializza
+    pub fn deserialize(&self){
+
+    }
+
+    pub fn unwrap(&self) -> Vec<GrammarRuleSec>{
+        match self{
+            GrammarRule::Raw(_)=> panic!("Error: before extracting the rules, 
+                                        it is necessary to deserialise them"),
+            GrammarRule::Worked(value)=> value.clone()
+
+        }
+    }
+}
+
+//Probabilmente da inserire un enum grammar section che permetta
+//Di inserire o una componente specifico o piu' possibili componenti
+#[derive(Clone, Debug)]
+pub enum GrammarRuleSec{
+    SingleRule(TokenKind),
+    MultipleChoise(Vec<TokenKind>)
+}
+
+impl GrammarRuleSec{
+    pub fn unwrap(&self){
+
+    }
+}
+
+
+
