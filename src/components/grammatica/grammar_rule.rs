288745e16ae632724275593a4c3fd7361971054e
Super User <root@fedora.homenet.telecomitalia.it>
2024-10-13 19:29:20 +0200

    grammatica
    scrittura parziale con fix di bug delle struct relative alla gestione locale della grammatica del linguaggio per cui si e' resa necessaria una riprogettazione. Implementazione del file per far risiedere i test per le nuove struct

--- src/components/grammatica/grammar_rule.rs
+++ src/components/grammatica/grammar_rule.rs
@@ -5,7 +5,11 @@ use crate::components::{lexer::*, parser::*};
 #[derive(Clone, Debug)]
 pub enum GrammarRule{
     Raw(String),
-    Worked(Vec<GrammarRuleSec>)
+    //Non posso lavorare con vec, ma serve una
+    //struttura che dia la possibilita' di legare
+    //tra loro piu' elementi
+    Worked(GrammarRuleSection),
+
 }
 
 impl GrammarRule{
@@ -16,32 +20,54 @@ impl GrammarRule{
     //Quando le regole vengono scaricate, sono in
     //un formato stringa. Questa funzione le deserializza
     pub fn deserialize(&self){
+        let binding = self.unwrap_raw_rules();
+        let rules = binding.as_str()
+                           .split('/');
+                        
+        for rule in rules{
+            if rule.to_string().contains('|'){
 
+            }else{
+
+            }
+        }
     }
 
-    pub fn unwrap(&self) -> Vec<GrammarRuleSec>{
+    pub fn unwrap(&self) -> GrammarRuleSection{
         match self{
-            GrammarRule::Raw(_)=> panic!("Error: before extracting the rules, 
-                                        it is necessary to deserialise them"),
+            GrammarRule::Raw(_)=> panic!("You can't unwrap the rules before you've deseriaalized them"),
             GrammarRule::Worked(value)=> value.clone()
 
         }
     }
-}
 
-//Probabilmente da inserire un enum grammar section che permetta
-//Di inserire o una componente specifico o piu' possibili componenti
-#[derive(Clone, Debug)]
-pub enum GrammarRuleSec{
-    SingleRule(TokenKind),
-    MultipleChoise(Vec<TokenKind>)
-}
-
-impl GrammarRuleSec{
-    pub fn unwrap(&self){
-
+    pub fn unwrap_raw_rules(&self) -> String{
+        match self{
+            GrammarRule::Raw(raw_rule)=> raw_rule.clone(),
+            _ => panic!("You can't unwrap non-raw rules with this method. Try the unwrap() method")
+        }
     }
 }
 
+#[derive(Clone, Debug)]
+enum GrammarRuleSection{
+	SingleToken(TokenKind),
+	MultipleTokens(Vec<TokenKind>)
+}
 
+impl GrammarRuleSection{
 
+    pub fn unwrap_single_token(&self) -> TokenKind{
+        match self{
+            GrammarRuleSection::SingleToken(token) => token.clone(),
+            _ => panic!("You can't unwrap non-single token rules section with this method. Try the unwrap_multiple_tokens() method")
+        }
+    }
+
+    pub fn unwrap_multiple_tokens(&self) -> Vec<TokenKind>{
+        match self{
+            GrammarRuleSection::MultipleTokens(tokens) => tokens.clone(),
+            _ => panic!("You can't unwrap non-multiple tokens rules section with this method. Try the unwrap_single_token() method")
+        }
+    }
+}
--- src/tests.rs
+++ src/tests.rs
@@ -1,2 +1,3 @@
 pub mod lexer_test;
 pub mod parser_test;
+pub mod grammar_test;
