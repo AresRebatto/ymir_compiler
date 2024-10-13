3d6e0ca9d6e7e5def41ddfce428a2ccbd4ba7f3d
Super User <root@fedora.homenet.telecomitalia.it>
2024-10-13 21:42:08 +0200

    da riorganizzare tutta parte grammatica

--- src/components/grammatica/grammar.rs
+++ src/components/grammatica/grammar.rs
@@ -1,10 +1,26 @@
 use serde::Deserialize;
 
+use crate::TokenKind;
+
 #[derive(Deserialize, Debug)]
 pub struct Grammar{
-	pub program: String,
-	pub instruction: String,
-	pub declaration: String,
-	pub assignment: String,
-	pub expression: String 
+	pub program: Vec<RuleFormat>,
+	pub instruction: Vec<RuleFormat>,
+	pub declaration: Vec<RuleFormat>,
+	pub assignment: Vec<RuleFormat>,
+	pub expression: Vec<RuleFormat> 
 }
+
+
+
+impl Grammar{
+	// pub fn new() -> Self{
+	// }
+}
+
+pub enum RuleFormat{
+	SingleToken(TokenKind),
+	PultipleTokens(Vec<TokenKind>),
+	GrammarRule(Grammar)
+}
+
