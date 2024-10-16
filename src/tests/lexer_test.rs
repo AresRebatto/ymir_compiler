979449cdf8f375e2222a30002ebcf3d0a424eea2
AresRebatto <aresrebatt@gmail.com>
2024-10-16 12:24:52 +0200

    test lexer e parser. Nei test lexer l'ultimo fallisce per motivi noti, il penultimo panica per motivo sconosciuto.

--- src/tests/lexer_test.rs
+++ src/tests/lexer_test.rs
@@ -22,3 +22,128 @@ fn get_tokens_i(){
 		}
 	]);
 }
+
+#[test]
+fn get_tokens_int_plus_equal() {
+	let res = get_tokens(String::from("int + = +="));
+	assert_eq!(res, [
+		Token{
+			token: TokenKind::Type(String::from("int")),
+			next_token: Some(TokenKind::Plus)
+		},
+		Token{
+			token: TokenKind::Plus,
+			next_token: Some(TokenKind::Equal)
+		},
+		Token{
+			token: TokenKind::Equal,
+			next_token: Some(TokenKind::Identifier(String::from("+=")))
+		},
+		Token{
+			token: TokenKind::Identifier(String::from("+=")),
+			next_token: None
+		}
+	]);
+}
+
+#[test]
+fn get_tokens_int_equal_42() {
+	let res = get_tokens(String::from("int = 42"));
+	assert_eq!(res, [
+		Token{
+			token: TokenKind::Type(String::from("int")),
+			next_token: Some(TokenKind::Equal)
+		},
+		Token{
+			token: TokenKind::Equal,
+			next_token: Some(TokenKind::Int(42))
+		},
+		Token{
+			token: TokenKind::Int(42),
+			next_token: None
+		}
+	]);
+}
+
+#[test]
+fn get_tokens_identifier_plus_minus() {
+	let res = get_tokens(String::from("x + -"));
+	assert_eq!(res, [
+		Token{
+			token: TokenKind::Identifier(String::from("x")),
+			next_token: Some(TokenKind::Plus)
+		},
+		Token{
+			token: TokenKind::Plus,
+			next_token: Some(TokenKind::Minus)
+		},
+		Token{
+			token: TokenKind::Minus,
+			next_token: None
+		}
+	]);
+}
+
+#[test]
+fn get_tokens_42_plus_x() {
+	let res = get_tokens(String::from("42 + x"));
+	assert_eq!(res, [
+		Token{
+			token: TokenKind::Int(42),
+			next_token: Some(TokenKind::Plus)
+		},
+		Token{
+			token: TokenKind::Plus,
+			next_token: Some(TokenKind::Identifier(String::from("x")))
+		},
+		Token{
+			token: TokenKind::Identifier(String::from("x")),
+			next_token: None
+		}
+	]);
+}
+
+#[test]
+fn get_tokens_multiple_statements() {
+	let res = get_tokens(String::from("int x = 42; x + 1"));
+	assert_eq!(res, 
+		Token{
+			token: TokenKind::Type(String::from("int")),
+			next_token: Some(TokenKind::Identifier(String::from("x")))
+		},
+		Token{
+			token: TokenKind::Identifier(String::from("x")),
+			next_token: Some(TokenKind::Equal)
+		},
+		Token{
+			token: TokenKind::Equal,
+			next_token: Some(TokenKind::Int(42))
+		},
+		Token{
+			token: TokenKind::Int(42),
+			next_token: Some(TokenKind::Semicolon)
+		},
+		Token{
+			token: TokenKind::Semicolon,
+			next_token: Some(TokenKind::Identifier(String::from("x")))
+		},
+		Token{
+			token: TokenKind::Identifier(String::from("x")),
+			next_token: Some(TokenKind::Plus)
+		},
+		Token{
+			token: TokenKind::Plus,
+			next_token: Some(TokenKind::Int(1))
+		},
+		Token{
+			token: TokenKind::Int(1),
+			next_token: None
+		}
+	]);
+}
+
+#[test]
+fn get_tokens_empty_string() {
+	let res = get_tokens(String::from(""));
+	assert_eq!(res, []);
+}
--- src/tests/parser_test.rs
+++ src/tests/parser_test.rs
@@ -13,6 +13,106 @@ fn ast_test_i(){
     ])
 }
 
+#[test]
+fn ast_test_int() {
+    let ast = get_ast(get_tokens("int".to_string()));
+
+    assert_eq!(ast, [
+        ASTNode{
+            token: Token{
+                token: TokenKind::Type("int".to_string()), next_token: None
+            },
+            linked_token: vec![TokenKind::Identifier("".to_string())]
+        }
+    ]);
+}
+
+#[test]
+fn ast_test_int_value() {
+    let ast = get_ast(get_tokens("42".to_string()));
+
+    assert_eq!(ast, [
+        ASTNode{
+            token: Token{
+                token: TokenKind::Int(42), next_token: None
+            },
+            linked_token: vec![TokenKind::Identifier("".to_string()), TokenKind::Minus, TokenKind::Plus]
+        }
+    ]);
+}
+
+#[test]
+fn ast_test_equal() {
+    let ast = get_ast(get_tokens("=".to_string()));
+
+    assert_eq!(ast, [
+        ASTNode{
+            token: Token{
+                token: TokenKind::Equal, next_token: None
+            },
+            linked_token: vec![TokenKind::Identifier("".to_string()), TokenKind::Int(0)]
+        }
+    ]);
+}
+
+#[test]
+fn ast_test_minus() {
+    let ast = get_ast(get_tokens("-".to_string()));
+
+    assert_eq!(ast, [
+        ASTNode{
+            token: Token{
+                token: TokenKind::Minus, next_token: None
+            },
+            linked_token: vec![TokenKind::Int(0), TokenKind::Identifier("".to_string())]
+        }
+    ]);
+}
+
+#[test]
+fn ast_test_plus() {
+    let ast = get_ast(get_tokens("+".to_string()));
+
+    assert_eq!(ast, [
+        ASTNode{
+            token: Token{
+                token: TokenKind::Plus, next_token: None
+            },
+            linked_token: vec![TokenKind::Int(0), TokenKind::Identifier("".to_string())]
+        }
+    ]);
+}
+
+#[test]
+fn ast_test_identifier() {
+    let ast = get_ast(get_tokens("x".to_string()));
+
+    assert_eq!(ast, [
+        ASTNode{
+            token: Token{
+                token: TokenKind::Identifier("x".to_string()), next_token: None
+            },
+            linked_token: vec![TokenKind::Minus, TokenKind::Plus, TokenKind::Equal]
+        }
+    ]);
+}
+
+#[test]
+fn ast_test_semicolon() {
+    let ast = get_ast(get_tokens(";".to_string()));
+
+    assert_eq!(ast, [
+        ASTNode{
+            token: Token{
+                token: TokenKind::Semicolon, next_token: None
+            },
+            linked_token: vec![]
+        }
+    ]);
+}
+
+
+
 
 
 
